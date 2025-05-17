396d640c5b1bc678fe974ab78208d057bd4e81a6
jezrel jumwa <jezrel.mwaviswa@ncbagroup.com>
2025-05-17 16:25:47 +0300

    First test case

--- src/leetcode/containsDuplicates.java
+++ src/leetcode/containsDuplicates.java
@@ -1,8 +1,30 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * @Created 17/05/2025
  * @Project data-structures-algorithms
  * @author jezreljumwa
-*/public class containsDuplicates {
+ */
+public class containsDuplicates {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+
+        }
+
+        if (set.size() < nums.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        containsDuplicates solution = new containsDuplicates();
+        System.out.println(solution.containsDuplicate(nums));
+    }
 }
